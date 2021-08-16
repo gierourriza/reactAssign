8c0daae6c034f5b74da178c8f90a3cd98276dd72
Rogimer Urriza <rurriza@village88.com>
2021-08-16 21:35:09 +0800

    Added other images js files

---
+++ src/components/others/NorteImages.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+class NorteImg extends Component {
+    render() {
+        return (
+            <div className="colTwo">
+                <h1>Lakbay, Ilocos Norte!</h1>
+                <img src="https://th.bing.com/th/id/OIP.rlnEoQY7t7Hso4fhpC1EOAHaEe?w=270&h=180&c=7&o=5&pid=1.7" alt="Ilocos Norte"/>
+                <img src="https://th.bing.com/th/id/OIP.G06yBq0Fkl7SzR-C9ZRr_wHaE-?w=242&h=180&c=7&o=5&pid=1.7" alt="Ilocos Norte"/>
+                <img src="https://th.bing.com/th/id/OIP.P1_-M2Zk2iNJdM7OVN9eqAHaE8?w=208&h=180&c=7&o=5&pid=1.7" alt="Ilocos Norte"/>
+                <img src="https://th.bing.com/th/id/OIP.Ny98WcRS_CUw9_HrsHhp-QHaE8?w=292&h=194&c=7&o=5&pid=1.7" alt="Ilocos Norte"/>
+                <img src="https://th.bing.com/th/id/OIP.TlkMPjtL_PZK3nipYovK_AHaE8?w=260&h=180&c=7&o=5&pid=1.7" alt="Ilocos Norte"/>
+                <img src="https://th.bing.com/th/id/OIP.GyRE2Xyl018WgnmBy2T6JgHaDe?w=272&h=164&c=7&o=5&pid=1.7" alt="Ilocos Norte"/>
+                <img src="https://th.bing.com/th/id/OIP.yxQV6usn_Oaw_iCWXs8KSAHaDt?w=316&h=175&c=7&o=5&pid=1.7" alt="Ilocos Norte"/>
+                <img src="https://th.bing.com/th/id/OIP.pmh3_uoGB_1xnaVTbIW7-AHaE8?w=249&h=180&c=7&o=5&pid=1.7" alt="Ilocos Norte"/>
+                
+            </div>
+        );
+    };
+};
+export default NorteImg;
---
+++ src/components/others/SubicImages.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+class SubicImg extends Component {
+    render() {
+        return (
+            <div className="colTwo">
+                <h1>Subic Bay. Where fun and nature collide</h1>
+                <img src="https://static.rappler.com/images/sbma-20130617.jpg" alt="Subic"/>
+                <img src="https://th.bing.com/th/id/R.d2c2457e34c8c12f8fba69023c068e94?rik=I4sFUAL62YhKcg&riu=http%3a%2f%2fwww.subic.com%2fimg%2fblog%2ftop10-2.jpg&ehk=PKaxIGtKyzPKCTc2Qf0%2fhKCJlZ3YThIueXj%2bv2Yni2I%3d&risl=&pid=ImgRaw&r=0" alt="Subic"/>
+                <img src="https://th.bing.com/th/id/R.fd9561601764429b58bcc5d6060bdb18?rik=YaXxoKnbIxMaSA&riu=http%3a%2f%2fwww.jeepneyguide.com%2fsites%2fdefault%2ffiles%2fsubic.jpg&ehk=h8HEw2XHlBOX%2fxSfCjQ0nJDGYEQocy6ncZzwAcZowpg%3d&risl=&pid=ImgRaw&r=0" alt="Subic"/>
+                <img src="https://www.vigattintourism.com/assets/article_main_photos/optimize/1348479974WiwccsyM.jpg" alt="Subic"/>
+                <img src="https://www.e-philippines.com.ph/wp-content/uploads/2020/06/Ocean-Adventure-Subic-Bay-Olongapo-Philippines-Adventure-travel-and-tour-promo-packages-3-635x422.jpg" alt="Subic"/>
+                <img src="https://th.bing.com/th/id/OIP.t1l-praqCTEn9nlkKzL1iQHaE8?w=266&h=180&c=7&o=5&pid=1.7" alt="Subic"/>
+                <img src="https://th.bing.com/th/id/OIP.mfAL1Yh2ZwDKg69iCazbcQHaE8?w=278&h=186&c=7&o=5&pid=1.7" alt="Subic"/>
+                <img src="https://th.bing.com/th/id/OIP.mpqa8cZV9dQjgITLM4v34QHaFj?w=248&h=186&c=7&o=5&pid=1.7" alt="Subic"/>
+                
+            </div>
+        );
+    };
+};
+export default SubicImg;
