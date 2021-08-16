8f955fc044d17df07744a16cb3628b3a613dfa48
Rogimer Urriza <rurriza@village88.com>
2021-08-16 20:54:10 +0800

    added new file boholimages

---
+++ src/components/others/BoholImages.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+class BoholImg extends Component {
+    render() {
+        return (
+            <div className="colTwo">
+                <h1>Behold... Bohol</h1>
+                <img src="https://th.bing.com/th/id/OIP.Ilg1Fjyzd3CI0UG2NWZ2tgHaE5?pid=ImgDet&rs=1" alt="Bohol"/>
+                <img src="https://th.bing.com/th/id/OIP.Mqkgbj_Y5AntkCzh59Jw4gHaE7?pid=ImgDet&rs=1" alt="Bohol"/>
+                <img src="https://th.bing.com/th/id/OIP.LI6llH3zignVMADe9mC1JwHaGT?pid=ImgDet&rs=1" alt="Bohol"/>
+                <img src="https://th.bing.com/th/id/OIF.4MbL33JyDN0RUTLD2Ajh4w?pid=ImgDet&rs=1" alt="Bohol"/>
+                <img src="https://farm3.staticflickr.com/2415/2817328988_ce174e72f9_b.jpg" alt="Bohol"/>
+                <img src="https://wpcdn.hotelhosting.co/wp-content/uploads/sites/719/2015/12/be-grand-resort-bohol-header-the-villas-2017-00.jpg" alt="Bohol"/>
+                <img src="https://th.bing.com/th/id/OIP.FBwDEbV8CsZ7jNWDV5AEuAHaEc?pid=ImgDet&rs=1" alt="Bohol"/>
+                <img src="https://unexploredfootsteps.com/wp-content/uploads/2018/04/img_2111-2-1440x1080.jpg" alt="Bohol"/>
+                
+            </div>
+        );
+    };
+};
+export default BoholImg;
